3294719090081c7285a111a545fde3ca06503761
anuragborkar2005 <anuragborkar2005@gmail.com>
2025-04-02 22:31:02 +0530

    added speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
@@ -29,6 +30,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
