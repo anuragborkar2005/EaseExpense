16d338839d4585f5c69e7794d58137e3b9b21266
anuragborkar2005 <anuragborkar2005@gmail.com>
2025-04-02 22:07:59 +0530

    Update history/page.tsx

--- app/dashboard/history/page.tsx
+++ app/dashboard/history/page.tsx
@@ -294,7 +294,7 @@ export default function HistoryPage() {
                           </TableCell>
                           <TableCell>
                             <span
-                              className={`inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium ${getCategoryColor(
+                              className={`inline-flex items-center rounded-full px-2.5 py-0.5 min-w-[150px] text-xs text-truncate font-medium ${getCategoryColor(
                                 expense.category
                               )}`}
                             >
